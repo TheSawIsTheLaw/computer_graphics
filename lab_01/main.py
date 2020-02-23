e409b6bccc8999ec84848e7abbab7c305ec4541c
xAMATEURx <dimakrok@bk.ru>
2020-02-24 02:35:38 +0300

    Boje, ubeite menya

--- lab_01/main.py
+++ lab_01/main.py
@@ -1,7 +1,8 @@
 from tkinter import *
+from re import *
 
-def printDebug():
-    print('666')
+def printDebug(var):
+    print(var)
 
 def makeJobWindow():
     jobWindow = Tk()
@@ -54,7 +55,65 @@ def makeCanvasStart(canvasWindow):
 
     canvasWindow.create_text(525, 485, text="0")
 
+def generateInputErrorWindow():
+    errorWindow = Tk()
+    errorWindow.title("Ошибка!")
+    errorLabel = Label(errorWindow, text="Должно быть введено два вещественных значения в следующем формате"
+                                         ":\n'x y'\nПовторите попытку, пожалуйста.", font=("consolas", 15))
+    errorLabel.pack()
 
+def generateInputRepeatErrorWindow():
+    errorWindow = Tk()
+    errorWindow.title("Ошибка!")
+    errorLabel = Label(errorWindow,
+                       text="Значения уже имеются в таблице.",
+                       font=("consolas", 15))
+    errorLabel.pack()
+
+def addPoint(pointArray, entry, listBox):
+    inputString = entry.get()
+    inputString = findall(r"[-+]?\d*\.\d+|\d+", inputString)
+    for i in range(len(inputString)):
+        inputString[i] = float(inputString[i])
+    #print(inputString)
+    if len(inputString) != 2:
+        generateInputErrorWindow()
+        return
+    if inputString not in pointArray:
+        listBox.insert(listBox.size() + 1, "X = " + str(inputString[0]) + "; Y = " + str(inputString[1]))
+        pointArray.append(inputString)
+    else:
+        generateInputRepeatErrorWindow()
+
+def generateEmptyError():
+    errorWindow = Tk()
+    errorWindow.title("Ошибка!")
+    errorLabel = Label(errorWindow,
+                       text="Не выбрано ни одного значения в таблице!",
+                       font=("consolas", 15))
+    errorLabel.pack()
+
+def deletePoint(pointArray, listBox):
+    if not listBox.curselection():
+        generateEmptyError()
+        return
+
+    delNum = tuple(listBox.curselection())[0]
+
+    listBox.delete(delNum)
+    pointArray.pop(delNum)
+
+#def changePoint(pointArray, entry, listBox):
+#    delPoint(...) НО ТРЕБУЕТСЯ ВСТАВЛЯТЬ ПО ИНДЕКСУ!!!
+#    addPoint(...)
+#
+#
+
+def makeVarZero(var):
+    var = 0
+
+def makeVarOne(var):
+    var = 1
 
 def makeMainWindow():
     rootWindow = Tk()
@@ -62,43 +121,60 @@ def makeMainWindow():
     rootWindow.minsize(1800, 1080)
 
     makeCascadeMenu(rootWindow)
+    firstDotsArray = []
+    secondDotsArray = []
 
     boolVar = BooleanVar()
+    choiceVar = 0
     boolVar.set(0)
-    radioButtonF = Radiobutton(text='Треугольник строится на точках одного множества', font=("consolas", 12), variable=boolVar, value=0, command=printDebug).grid(row=0, column=0, columnspan=3)
-    radioButtonS = Radiobutton(text='Треугольник строится на точках разных множеств', font=("consolas", 12), variable=boolVar, value=1, command=printDebug).grid(row=1, column=0, columnspan=3)
+    radioButtonF = Radiobutton(text='Треугольник строится на точках одного множества', font=("consolas", 12),
+                               variable=boolVar, value=0, command=lambda: makeVarZero(choiceVar)).grid(row=0,
+                                                                                                       column=0,
+                                                                                                       columnspan=3)
+    radioButtonS = Radiobutton(text='Треугольник строится на точках разных множеств', font=("consolas", 12),
+                               variable=boolVar, value=1, command=lambda: makeVarOne(choiceVar)).grid(row=1,
+                                                                                                      column=0,
+                                                                                                      columnspan=3)
 
-    listLableFirst = Label(rootWindow, text="Множество 1:", font=("consolas", 20), fg="white", bg="black").grid(row=3, column=0, columnspan=2)
+    listLableFirst = Label(rootWindow, text="Множество 1:", font=("consolas", 20), fg="white",
+                           bg="black").grid(row=3,
+                                            column=0,
+                                            columnspan=2)
 
     listBoxFirst = Listbox(rootWindow, height=15, width=30, selectmode=SINGLE)
-    listBoxFirst.insert(0, "0, 0.11")
     listBoxFirst.grid(row=4, column=2, columnspan=2, rowspan=5)
-    listBoxFirst.insert(0, 0)
-    entryFirstLable = Label(rootWindow, text="Добавляемая в первое множество вершина:")
+    entryFirstLable = Label(rootWindow, text="Добавляемая вершина (x, y через пробел):")
     entryFirstLable.grid(row=5, column=0)
     entryFirst = Entry(rootWindow)
     entryFirst.grid(row=5, column=1)
-    insertButtonFirst = Button(rootWindow, text="Добавить", width=30)
+    insertButtonFirst = Button(rootWindow, text="Добавить", width=30, command=lambda: addPoint(firstDotsArray,
+                                                                                               entryFirst,
+                                                                                               listBoxFirst))
     insertButtonFirst.grid(row=6, column=0, columnspan=2)
-    delButtonFirst = Button(rootWindow, text="Удалить выбранную в списке точку (из множества 1)")
+    delButtonFirst = Button(rootWindow, text="Удалить выбранную в списке точку (из множества 1)", command= lambda: deletePoint(firstDotsArray, listBoxFirst))
     delButtonFirst.grid(row=7, column=0, columnspan=2)
     changeButtonFirst = Button(rootWindow, text="Изменить выбранную в списке точку (из множества 1)")
     changeButtonFirst.grid(row=8, column=0, columnspan=2)
 
     emptyLabel1 = Label(rootWindow, text="---------------------------------------------------------------------------"
-                                        "----------------------------------------------")
+                                         "----------------------------------------------")
     emptyLabel2 = Label(rootWindow, text="---------------------------------------------------------------------------"
                                          "----------------------------------------------")
     emptyLabel1.grid(row=9, columnspan=3)
     emptyLabel2.grid(row=2, columnspan=3)
-    listLableSecond = Label(rootWindow, text="Множество 2:", font=("consolas", 20), fg="white", bg="black").grid(row=10, column=0, columnspan=2)
+    listLableSecond = Label(rootWindow, text="Множество 2:", font=("consolas", 20), fg="white",
+                            bg="black").grid(row=10,
+                                             column=0,
+                                             columnspan=2)
     listBoxSecond = Listbox(rootWindow, height=15, width=30, selectmode=SINGLE)
     listBoxSecond.grid(row=11, column=2, columnspan=2, rowspan=5)
-    entrySecondLable = Label(rootWindow, text="Добавляемая во второе множество вершина:")
+    entrySecondLable = Label(rootWindow, text="Добавляемая вершина (x, y через пробел):")
     entrySecondLable.grid(row=12, column=0)
     entrySecond = Entry(rootWindow)
     entrySecond.grid(row=12, column=1)
-    insertButtonSecond = Button(rootWindow, text="Добавить", width=30)
+    insertButtonSecond = Button(rootWindow, text="Добавить", width=30, command=lambda: addPoint(secondDotsArray,
+                                                                                                entrySecond,
+                                                                                                listBoxSecond))
     insertButtonSecond.grid(row=13, column=0, columnspan=2)
     delButtonSecond = Button(rootWindow, text="Удалить выбранную в списке точку (из множества 2)")
     delButtonSecond.grid(row=14, column=0, columnspan=2)
@@ -116,3 +192,4 @@ def makeMainWindow():
     rootWindow.mainloop()
 
 makeMainWindow()
+
