47e84a911e4695a481f78111be0b3050b29c4a94
xAMATEURx <dimakrok@bk.ru>
2020-02-23 15:08:54 +0300

    Oh...

--- lab_01/main.py
+++ lab_01/main.py
@@ -1,9 +1,35 @@
 from tkinter import *
 
-rootWindow = Tk()
-rootWindow.minsize(1880, 1080)
+def printDebug():
+    print('666')
 
-helloLabel = Label(rootWindow, text="Задание:", font=("Consolas", 14), width=20)
-helloLabel.pack() # side=TOP, padx=10, pady=10
+def makeJobWindow():
+    jobWindow = Tk()
 
-rootWindow.mainloop()
+    jobWindow.minsize(400, 400)
+    jobLabel = Label(jobWindow, text="На плоскости даны два...")
+    jobLabel.pack()
+
+    jobWindow.mainloop()
+
+def makeCascadeMenu(rootWindow):
+    rootMenu = Menu(rootWindow)
+    rootWindow.config(menu=rootMenu)
+
+    jobMenu = Menu(rootMenu)
+    jobMenu.add_command(label='Формулировка', command=makeJobWindow)
+    moreMenu = Menu(rootMenu)
+    moreMenu.add_command(label='Сбросить всё', command=printDebug)
+
+    rootMenu.add_cascade(label='Задание', menu=jobMenu)
+    rootMenu.add_cascade(label='Дополнительные возможности', menu=moreMenu)
+
+def makeMainWindow():
+    rootWindow = Tk()
+    rootWindow.minsize(1880, 1080)
+
+    makeCascadeMenu(rootWindow)
+
+    rootWindow.mainloop()
+
+makeMainWindow()
