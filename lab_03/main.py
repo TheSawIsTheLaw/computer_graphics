6554f33e28ebccb918aaf2074d7e40a0f240e8a5
xAMATEURx <dimakrok@bk.ru>
2020-04-01 17:13:04 +0300

    Start: combobox, two entrys for lines and degrees for the sun were added

--- lab_03/main.py
+++ lab_03/main.py
@@ -50,23 +50,47 @@ def makeMainWindow():
     """
     rootWindow = Tk()
     rootWindow.title("Рабораторная работа 3, Якуба Дмитрий, ИУ7-43Б")
-    rootWindow.configure(background = "gray")
-    rootWindow.minsize(1800, 920)
-    listBox = ttk.Combobox(rootWindow)
-    listBox.grid()
+    rootWindow.geometry("1850x1080+60+0")
+
+    # Выбор метода построения
+    Label(rootWindow, text = "Метод построения:", font = fontSettingLower).grid(row = 0, column = 0, columnspan = 1)
+    listBox = ttk.Combobox(rootWindow, width = 60)
+    listBox.grid(row = 0, column = 1, columnspan = 4)
+
+    Label(rootWindow, text = "Начальная точка отрезка", font = fontSettingLower).grid(row = 1, columnspan = 5)
+    Label(rootWindow, text = "Координата X:", font = fontSettingLower).grid(row = 2)
+    Label(rootWindow, text = "Координата Y:", font = fontSettingLower).grid(row = 2, column = 2)
+    entryXFirst = Entry(rootWindow, font = fontSettingLower, width = 5).grid(row = 2, column = 1)
+    entryYFirst = Entry(rootWindow, font = fontSettingLower, width = 5).grid(row = 2, column = 3)
+
+    Label(rootWindow, text = "Конечная точка отрезка", font = fontSettingLower).grid(row = 3, columnspan = 5)
+    Label(rootWindow, text = "Координата X:", font = fontSettingLower).grid(row = 4)
+    Label(rootWindow, text = "Координата Y:", font = fontSettingLower).grid(row = 4, column = 2)
+    entryXSecond = Entry(rootWindow, font = fontSettingLower, width = 5).grid(row = 4, column = 1)
+    entryYSecond = Entry(rootWindow, font = fontSettingLower, width = 5).grid(row = 4, column = 3)
+
+    Button(rootWindow, text = "Построить отрезок", font = fontSettingLower, command = print()).grid(row = 5, columnspan = 5)
+
+    Label(rootWindow, text = "Угол поворота при построении каждого отрезка (в градусах): ", font = fontSettingLower).grid(row = 6, columnspan = 4)
+    Entry(rootWindow, font = fontSettingLower).grid(row = 6, column = 4)
 
     canvasWindow = Canvas(rootWindow, bg = "white", width = 1075, height = 1055)
-    canvasWindow.grid(row = 0, column = 3, rowspan = 20)
+    canvasWindow.grid(row = 0, column = 6, rowspan = 20)
 
+    ''' QUICKLY
+        Button(rootWindow, text = "", font = fontSettingLower, command = print()).grid(row = , column = , columnspan =, rowspan = )
+        Entry(rootWindow, font = fontSettingLower).grid(row = , column = , columnspan =, rowspan = )
+        Label(rootWindow, text = "", font = fontSettingLower).grid(row = , column = , columnspan =, rowspan = )
+    '''
     # EXAMPLE FOR PIXEL WORK
     # img = PhotoImage(width = 1075, height = 1055)
-    # canvasWindow.create_image((1075 / 2, 1055 / 2), image=img, state = "normal")
+    # canvasWindow.create_image((1075, 1055
+    # ), image=img, state = "normal")
     # for i in range(100):
     #     img.put("black", (500 + i, 500 - i))
 
     makeCascadeMenu(rootWindow)
 
     rootWindow.mainloop()
-
 
 makeMainWindow()
